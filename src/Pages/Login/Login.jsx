ea270c42db6914e81354a4c3a004c1383432ed53
muna564 <munaandkhan@gmail.com>
2023-10-04 16:23:20 +0600

    added the login page

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,59 @@
+import { Link } from "react-router-dom";
+import Navbar from "../shared/Navbar/Navbar";
+
+const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(e.currentTarget);
+    const form = new FormData(e.currentTarget);
+    console.log(form.get("password"));
+  };
+  return (
+    <div>
+      <Navbar></Navbar>
+      <h2 className="text-2xl my-10 text-center">Please login</h2>
+      <form onSubmit={handleLogin} className="md:w-3/4 lg:w-1/2 mx-auto">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            className="input input-bordered"
+            required
+          />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            className="input input-bordered"
+            required
+          />
+          <label className="label">
+            <a href="#" className="label-text-alt link link-hover">
+              Forgot password?
+            </a>
+          </label>
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Login</button>
+        </div>
+      </form>
+      <p className="text-center mt-4">
+        Do not have an account?{" "}
+        <Link to="/register" className="text-blue-600 underline font-bold">
+          Register
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
--- src/Pages/shared/Navbar/Navbar.jsx
+++ src/Pages/shared/Navbar/Navbar.jsx
@@ -13,6 +13,9 @@ const Navbar = () => {
       <li>
         <NavLink to="/career">Career</NavLink>
       </li>
+      <li>
+        <NavLink to="/login">Login</NavLink>
+      </li>
     </>
   );
   return (
@@ -52,7 +55,7 @@ const Navbar = () => {
             <img src={useDefaultPic} />
           </div>
         </label>
-        <Link>
+        <Link to="/login">
           <button className="btn">Login</button>
         </Link>
       </div>
