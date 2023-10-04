824a7b00ed6ee1ed1d188717d9187e17868be118
muna564 <munaandkhan@gmail.com>
2023-10-04 16:42:40 +0600

    added the register page

--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,5 +1,86 @@
+import { Link } from "react-router-dom";
+import Navbar from "../shared/Navbar/Navbar";
+
 const Register = () => {
-  return <div></div>;
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log(e.currentTarget);
+    const form = new FormData(e.currentTarget);
+    console.log(form.get("password"));
+  };
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div>
+        <h2 className="text-2xl my-10 text-center">Register your account</h2>
+        <form onSubmit={handleRegister} className="md:w-3/4 lg:w-1/2 mx-auto">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Your Name"
+              name="name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Photo URL</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Photo URL"
+              name="photo"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Register</button>
+          </div>
+        </form>
+        <p className="text-center mt-4">
+          Already have an account?
+          <Link to="/login" className="text-blue-600 underline font-bold">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
--- src/Pages/shared/Navbar/Navbar.jsx
+++ src/Pages/shared/Navbar/Navbar.jsx
@@ -13,9 +13,6 @@ const Navbar = () => {
       <li>
         <NavLink to="/career">Career</NavLink>
       </li>
-      <li>
-        <NavLink to="/login">Login</NavLink>
-      </li>
     </>
   );
   return (
